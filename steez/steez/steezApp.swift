329810c6847f6ce7f1cea99dab90ef1eba5b724f
Sthefanny Gonzaga <sthefannygonzaga@gmail.com>
2022-05-12 15:55:42 -0300

    starting project

---
+++ steez/steez/steezApp.swift
@@ -0,0 +1,17 @@
+//
+//  steezApp.swift
+//  steez
+//
+//  Created by Sthefanny Gonzaga on 12/05/22.
+//
+
+import SwiftUI
+
+@main
+struct steezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
