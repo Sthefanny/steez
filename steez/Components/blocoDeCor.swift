f1039cca21f85b76f200d59b7e6cc6c2f4ced9df
Deborah <deborahs.eberle@gmai.com>
2022-05-19 14:48:46 -0300

    nome do botão

--- steez/Components/blocoDeCor.swift
+++ steez/Components/blocoDeCor.swift
@@ -19,17 +19,6 @@ struct blocoDeCor: View {
             RoundedRectangle(cornerRadius: 5)
                 .frame(width: 98, height: 74, alignment: .center)
                 
-                Text ("Nome da Cor")
-                    .font(.system(size: 12))
-                    .foregroundColor(.brown)
-                    .fontWeight(.bold)
-                    .frame(width: 95, height: 10, alignment: .leading)
-                
-                Text ("velocidade")
-                    .font(.system(size: 12))
-                    .foregroundColor(.brown)
-                    .fontWeight(.regular)
-                    .frame(width: 95, height: 10, alignment: .leading)
                 
                     
                 
